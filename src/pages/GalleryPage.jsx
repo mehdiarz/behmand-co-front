ea5d99aade94b001e8b403c94cf0a946ffc1712e
mahdiarz <mahdi_arezoomand@yahoo.com>
2025-11-16 14:45:33 +0330

    adding better photos

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -36,7 +36,7 @@ import bg3 from "../assets/bg-building.jpg";
 import bg4 from "../assets/bg-1.jpg";
 import bg5 from "../assets/bg-2.jpg";
 import bg7 from "../assets/bg-4.jpg";
-import bg8 from "../assets/bg-5.jpg";
+// import bg8 from "../assets/bg-5.jpg";
 import bg6 from "../assets/bg-3.jpg";
 import bg9 from "../assets/bg-6.jpg";
 
@@ -66,7 +66,7 @@ const GALLERY_IMAGES = [
     gallery1,
     bg6,
     bg7,
-    bg8,
+    // bg8,
     bg9
 ];
 
